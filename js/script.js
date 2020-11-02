a94c54031a838f9246023537f1c2b7f7aca770b1
Vlad Bond <foolowin@gmail.com>
2020-11-02 12:25:02 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@
 
 
 // console.log(document.documentElement);
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastElementChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current="3').nextElementSibling);
+
+
+for (const node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
