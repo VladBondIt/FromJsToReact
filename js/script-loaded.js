355b93f7af6feba347eadbb7e5113c630c8e2795
Vlad Bond <foolowin@gmail.com>
2020-11-03 13:55:26 +0300

    lesson_35

---
+++ js/script-loaded.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let button = document.querySelectorAll('button');
+
+console.log(button);
+
+// defer
+// async
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript('js/reduce.js')
