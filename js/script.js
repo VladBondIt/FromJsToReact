2b7a6aab7009d0ec2a436703de0d852424b7098d
Vlad Bond <foolowin@gmail.com>
2020-11-03 16:35:19 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+let btns = document.querySelectorAll('.first button'),
+    wrapper = document.querySelector('.first');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(2));
+// console.log(btns[0].classList.add('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+// console.log(btns[0].classList.contains('blue'));
+
+// if (btns[0].classList.contains('red')) {
+
+// }
+
+// btns[0].addEventListener('click', () => {
+//     if (!btns[0].classList.contains('red')) {
+//         btns[0].classList.add('red');
+//     } else {
+//         btns[0].classList.remove('red')
+//     }
+// });
+
+wrapper.addEventListener('click', (e) => {
+    // if (e.target && e.target.tagName == 'BUTTON') {
+    //     console.log('Hello');
+    // }
+    if (e.target && e.target.classList.contains('red')) {
+        console.log('Hello');
+    }
+    // console.dir(e.target);
+})
