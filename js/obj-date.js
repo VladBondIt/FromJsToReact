9481eae68d01738cacea71995b8af99185ad4442
Vlad Bond <foolowin@gmail.com>
2020-11-04 16:43:49 +0300

    lesson-40

---
+++ js/obj-date.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+// const now = new Date(2020, 10, 4, 20);
+// const now = new Date('2020-11-04');
+// const now = new Date(-999999999999);
+
+// const now = new Date('2020-11-04');
+
+// new Date.parse('2020-11-04');
+
+// console.log(now.setHours(18, 40));
+// console.log(now.setHours(40));
+// console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getMilliseconds());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());   // !!! КОличество милисекунд прошедшее с даты 1970 1 1 
+
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+//     let some = 1 ** 3;
+// }
+
+// let end = new Date();
+
+// console.log(`Цикл отработал зв ${end - start} милисекунд`);
