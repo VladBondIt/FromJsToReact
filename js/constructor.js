bbf646a3e7006d82dd1ef665f5ab234c362500b1
Vlad Bond <foolowin@gmail.com>
2020-11-05 13:48:59 +0300

    lesson 45

---
+++ js/constructor.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
+
+// const vlad = new User('Vlad', 28);
+// const alex = new User('Alex', 25);
+
+// console.log(vlad, alex);
+// console.log(vlad.hello(), alex.hello());
+
+// User.prototype.exit = function () {
+//     console.log(`User ${this.name} is leave`);
+// };
+
+// console.log(vlad.exit());
+
+
+
+
+
