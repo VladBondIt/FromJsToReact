c0d6979e20a817a19d24008f7bb1835f2dd8821d
Vlad Bond <foolowin@gmail.com>
2020-11-26 19:57:22 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const a = {
+    data: [
+        { label: "sick", id: "adsad" },
+        { label: "lick", id: "adsdad" },
+        { label: "mick", id: "adaszxd" },
+        { label: "dick", id: "adsvzxc" }
+    ]
+}
+
+const { data: [, , { id }] } = a;
+
+console.log(id);
+const newA = a.data.filter(x => x.id !== id ? x : null).map(x => x);
+
+console.log(newA);
+console.log(a.data);
