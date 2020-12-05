8d5af4ac57926a1d87abe24eb5f98347a595b00d
Vlad Bond <foolowin@gmail.com>
2020-12-05 15:16:53 +0300

    practice

--- js/closure-task.js
+++ js/closure-task.js
@@ -1,17 +1,19 @@
 'use strict';
 
 
-function sum(num) {
-    console.log(num);
-    return function (x) {
-        const z = num + x
-        console.log(z);
-        return function (c) {
-            const b = z + c
-            console.log(b);
-        };
-    }
-}
+// function sum(num) {
+//     console.log(num);
+//     return function (x) {
+//         const z = num + x;
+//         console.log(z);
+//         return function (c) {
+//             const b = z + c;
+//             console.log(b);
+//         };
+//     }
+// }
+
+const sum = num => x => c => console.log(num, x, c, c + (x + num));
 
 sum(1)(2)(3);
 
