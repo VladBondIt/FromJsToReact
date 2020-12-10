d34172f2fbb19bb6f91bb2b44662b58db26d26a7
Vlad Bond <foolowin@gmail.com>
2020-12-10 14:37:53 +0300

    tbc

--- js/codewars-tasks.js
+++ js/codewars-tasks.js
@@ -143,3 +143,22 @@ function solution(str) {
 console.log(solution("abcdefg"));
 
 
+function anagramm(str, otherStr) {
+    const tempStr = toSort(str);
+    const tempOtherStr = toSort(otherStr);
+
+    function toSort(str) {
+        const temp = str.toLowerCase();
+        return [...temp].sort().join('');
+    }
+    // console.log(tempStr);
+    // console.log(tempOtherStr);
+
+    if (tempStr === tempOtherStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(anagramm('Friend', 'Finder'));
--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+
+
+function anagramm(str, otherStr) {
+    const tempStr = toSort(str);
+    const tempOtherStr = toSort(otherStr);
+
+    function toSort(str) {
+        const temp = str.toLowerCase();
+        return [...temp].sort().join('');
+    }
+    // console.log(tempStr);
+    // console.log(tempOtherStr);
+
+    if (tempStr === tempOtherStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(anagramm('Friend', 'Finder'));
