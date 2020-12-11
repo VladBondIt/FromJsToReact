0d0e6660108bfc9f9dd7b0f8ab8d9d2acf7059b9
Vlad Bond <foolowin@gmail.com>
2020-12-11 14:51:51 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 
 
 
+function simpleNum(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return num > 1;
+}
+console.log(simpleNum(11));
