a025679e042be4d9b93ae8bdf70a1a96f458b384
Vlad Bond <foolowin@gmail.com>
2021-05-05 16:07:00 +0300

    ipv4 task

--- js/codewars-tasks.js
+++ js/codewars-tasks.js
@@ -279,3 +279,55 @@ function generateHashtag(str) {
 }
 
 generateHashtag("#A" + "a".repeat(138))
+
+// "10.0.0.0", "10.0.0.50"
+// "10.0.0.0", "10.0.1.0"
+// "20.0.0.10", "20.0.1.0"
+
+
+function ipsBetween(start, end) {
+    const arrStart = start.split('.')
+    const arrEnd = end.split('.')
+    let res = {
+        "0": 0,
+        "1": 0,
+        "2": 0,
+        "3": 0,
+    };
+
+    arrStart.forEach((value, i) => {
+        if (value !== arrEnd[i]) {
+            if (value < arrEnd[i]) {
+                for (let j = +value; j < +arrEnd[i]; j++) {
+                    res[i] += 1
+                }
+            } else {
+                res[i] = "m " + value
+            }
+        }
+    })
+
+    return Object.entries(res).reduce((acc, curr) => {
+        switch (true) {
+            case curr[0] === "0" && curr[1] !== 0:
+                acc += (65536 * 256) * curr[1]
+                return acc
+            case curr[0] === "1" && curr[1] !== 0:
+                acc += (256 ** 2) * curr[1]
+                return acc
+            case curr[0] === "2" && curr[1] !== 0:
+                acc += (curr[1] * 256)
+                return acc
+            case curr[0] === "3" && curr[1] !== 0:
+                if (curr[1][0] === 'm') {
+                    acc -= +curr[1].split(' ')[1]
+                } else {
+                    acc += curr[1]
+                }
+                return acc
+        }
+        return acc;
+    }, 0)
+}
+
+console.log(ipsBetween('1.2.3.4', '5.6.7.8'))
--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
 'use strict';
 
+// "10.0.0.0", "10.0.0.50"
+// "10.0.0.0", "10.0.1.0"
+// "20.0.0.10", "20.0.1.0"
 
-function add(n) {
-    const fn = function (x) {
-        return add(n + x);
+
+function ipsBetween(start, end) {
+    const arrStart = start.split('.')
+    const arrEnd = end.split('.')
+    let res = {
+        "0": 0,
+        "1": 0,
+        "2": 0,
+        "3": 0,
     };
 
-    fn.valueOf = function () {
-        return n;
-    };
+    arrStart.forEach((value, i) => {
+        if (value !== arrEnd[i]) {
+            if (value < arrEnd[i]) {
+                for (let j = +value; j < +arrEnd[i]; j++) {
+                    res[i] += 1
+                }
+            } else {
+                res[i] = "m " + value
+            }
+        }
+    })
 
-    return fn;
+    return Object.entries(res).reduce((acc, curr) => {
+        switch (true) {
+            case curr[0] === "0" && curr[1] !== 0:
+                acc += (65536 * 256) * curr[1]
+                return acc
+            case curr[0] === "1" && curr[1] !== 0:
+                acc += (256 ** 2) * curr[1]
+                return acc
+            case curr[0] === "2" && curr[1] !== 0:
+                acc += (curr[1] * 256)
+                return acc
+            case curr[0] === "3" && curr[1] !== 0:
+                if (curr[1][0] === 'm') {
+                    acc -= +curr[1].split(' ')[1]
+                } else {
+                    acc += curr[1]
+                }
+                return acc
+        }
+        return acc;
+    }, 0)
 }
+
+console.log(ipsBetween('1.2.3.4', '5.6.7.8'))
+
